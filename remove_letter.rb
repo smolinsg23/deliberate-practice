8fbdf4e65ce9d49355d1197dff791762cdf6fbd1
Sam S <SamS@tests-MacBook-Pro.local>
2016-11-06 14:59:16 -0600

    removes first letter

---
+++ remove_letter.rb
@@ -0,0 +1,23 @@
+#remove the first letter of each string in an array
+word = ["cat", "dog", "pig"]
+a = []
+
+word.each do |first|
+  first.slice!(0)
+  a << first
+end
+puts a
+
+
+
+
+#print out the second and fourth element of an array
+
+
+
+
+
+
+
+
+
